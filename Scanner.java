50793a63984521072cdd53b7644e098ac814951f
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2023-10-04 22:13:20 -0600

    Archivos principales del proyecto

---
+++ Scanner.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scanner {
+
+    private static final Map<String, TipoToken> palabrasReservadas;
+
+    static {
+        palabrasReservadas = new HashMap<>();
+        palabrasReservadas.put("and",    TipoToken.AND);
+        palabrasReservadas.put("else",   TipoToken.ELSE);
+        palabrasReservadas.put("false",  TipoToken.FALSE);
+        palabrasReservadas.put("for",    TipoToken.FOR);
+        palabrasReservadas.put("fun",    TipoToken.FUN);
+        palabrasReservadas.put("if",     TipoToken.IF);
+        palabrasReservadas.put("null",   TipoToken.NULL);
+        palabrasReservadas.put("or",     TipoToken.OR);
+        palabrasReservadas.put("print",  TipoToken.PRINT);
+        palabrasReservadas.put("return", TipoToken.RETURN);
+        palabrasReservadas.put("true",   TipoToken.TRUE);
+        palabrasReservadas.put("var",    TipoToken.VAR);
+        palabrasReservadas.put("while",  TipoToken.WHILE);
+    }
+
+    private final String source;
+
+    private final List<Token> tokens = new ArrayList<>();
+    
+    public Scanner(String source){
+        this.source = source + " ";
+    }
+
+    public List<Token> scan() throws Exception {
+        int estado = 0;
+        String lexema = "";
+        char c;
+
+        for(int i=0; i<source.length(); i++){
+            c = source.charAt(i);
+
+            switch (estado){
+                case 0:
+                    if(Character.isLetter(c)){
+                        estado = 13;
+                        lexema += c;
+                    }
+                    else if(Character.isDigit(c)){
+                        estado = 15;
+                        lexema += c;
+
+                        /*while(Character.isDigit(c)){
+                            lexema += c;
+                            i++;
+                            c = source.charAt(i);
+                        }
+                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
+                        lexema = "";
+                        estado = 0;
+                        tokens.add(t);
+                        */
+
+                    }
+                    break;
+
+                case 13:
+                    if(Character.isLetterOrDigit(c)){
+                        estado = 13;
+                        lexema += c;
+                    }
+                    else{
+                        TipoToken tt = palabrasReservadas.get(lexema);
+
+                        if(tt == null){
+                            Token t = new Token(TipoToken.IDENTIFIER, lexema);
+                            tokens.add(t);
+                        }
+                        else{
+                            Token t = new Token(tt, lexema);
+                            tokens.add(t);
+                        }
+
+                        estado = 0;
+                        lexema = "";
+                        i--;
+
+                    }
+                    break;
+
+                case 15:
+                    if(Character.isDigit(c)){
+                        estado = 15;
+                        lexema += c;
+                    }
+                    else if(c == '.'){
+
+                    }
+                    else if(c == 'E'){
+
+                    }
+                    else{
+                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
+                        tokens.add(t);
+
+                        estado = 0;
+                        lexema = "";
+                        i--;
+                    }
+                    break;
+            }
+
+
+        }
+
+
+        return tokens;
+    }
+}
