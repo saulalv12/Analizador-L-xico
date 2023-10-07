253be435ec4d336f45bd186ab043be8dae5f3f75
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2023-10-07 15:42:15 -0700

    Actualización del interprete
    Se realizan cambios en el código para que mande de forma correcta el mensaje de error que esta implementado en el scanner.

--- Interprete.java
+++ Interprete.java
@@ -1,3 +1,4 @@
+package interprete;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,13 +8,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Interprete {
-
     static boolean existenErrores = false;
-
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Uso correcto: interprete [archivo.txt]");
-
             // Convención defininida en el archivo "system.h" de UNIX
             System.exit(64);
         } else if(args.length == 1){
@@ -22,43 +20,34 @@ public class Interprete {
             ejecutarPrompt();
         }
     }
-
     private static void ejecutarArchivo(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         ejecutar(new String(bytes, Charset.defaultCharset()));
-
         // Se indica que existe un error
         if(existenErrores) System.exit(65);
     }
-
     private static void ejecutarPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
-
         for(;;){
-            System.out.print(">>> ");
+            System.out.print(">>>");
             String linea = reader.readLine();
             if(linea == null) break; // Presionar Ctrl + D
             ejecutar(linea);
             existenErrores = false;
         }
     }
-
     private static void ejecutar(String source) {
-        try{
-            Scanner scanner = new Scanner(source);
+        Scanner scanner = new Scanner(source); // Inicialización fuera del try
+        try {
             List<Token> tokens = scanner.scan();
-
-            for(Token token : tokens){
+            for (Token token : tokens) {
                 System.out.println(token);
             }
+        } catch (Exception ex) {
+            Interprete.error(scanner.linea, ex.getMessage());
         }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
-
     }
-
     /*
     El método error se puede usar desde las distintas clases
     para reportar los errores:
@@ -67,12 +56,10 @@ public class Interprete {
     static void error(int linea, String mensaje){
         reportar(linea, "", mensaje);
     }
-
     private static void reportar(int linea, String posicion, String mensaje){
         System.err.println(
                 "[linea " + linea + "] Error " + posicion + ": " + mensaje
         );
         existenErrores = true;
     }
-
-}
+} 
