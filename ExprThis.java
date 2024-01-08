28cbf2eecdaf92e8dc91308f02266ce1952b412c
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:09:53 -0600

    Update ExprThis.java
    Modificamos ExprThis agregando identación y en la evaluación de la tabla de símbolos retorna nulo

--- ExprThis.java
+++ ExprThis.java
@@ -6,4 +6,15 @@ public class ExprThis extends Expression{
     ExprThis() {
         // this.keyword = keyword;
     }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "ExprThis");
+    
+    }
+
+    @Override
+    public Object evaluate(TablaSimbolos tablita) {
+        return null;
+    }
 }
