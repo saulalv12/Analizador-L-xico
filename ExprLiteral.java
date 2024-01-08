4a3d0383e504811323ca2a63c0df0cd2d0792cf5
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-01-07 21:45:14 -0600

    método toString y print

--- ExprLiteral.java
+++ ExprLiteral.java
@@ -5,4 +5,19 @@ class ExprLiteral extends Expression {
     ExprLiteral(Object value) {
         this.value = value;
     }
+     @Override
+    public String toString() {
+        if (value instanceof String) {
+            // Asegúrate de que el valor no contenga comillas
+            return (String) value;
+        } else {
+            // Usa String.valueOf para convertir otros objetos a una cadena
+            return String.valueOf(value);
+        }
+    }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "ExprLiteral: " + value);
+    }
 }
