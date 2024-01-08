eebdd8e78c3ead06aee565566c04d0b794fbca7d
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2024-01-07 22:00:27 -0600

    Actualización de StmtIf
    Se agrega la función "print" y "toString" para lograr la impresión del árbol

--- StmtIf.java
+++ StmtIf.java
@@ -9,4 +9,34 @@ public class StmtIf extends Statement {
         this.thenBranch = thenBranch;
         this.elseBranch = elseBranch;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(if ").append(condition).append("\n");
+        builder.append("(then ").append(thenBranch).append(")\n");
+
+        if (elseBranch != null) {
+            builder.append("(else ").append(elseBranch).append(")\n");
+        }
+        
+        builder.append(")");
+        return builder.toString();
+    }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "StmtIf");
+        System.out.println(indentation + "\tCondition:");
+        condition.print(indentation + "\t\t");
+
+        System.out.println(indentation + "\tThen Branch:");
+        thenBranch.print(indentation + "\t\t");
+
+        if (elseBranch != null) {
+            System.out.println(indentation + "\tElse Branch:");
+            elseBranch.print(indentation + "\t\t");
+        }
+    }
 }
