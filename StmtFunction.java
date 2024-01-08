dac12281e4cece3e4e5e76f01872e862fed4eb16
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2024-01-07 22:01:18 -0600

    Actualización de StmtFunction
    Se agrega la función "print" para lograr la impresión del árbol

--- StmtFunction.java
+++ StmtFunction.java
@@ -13,4 +13,18 @@ public class StmtFunction extends Statement {
         this.params = params;
         this.body = body;
     }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "StmtFunction: " + name.lexema);
+
+        // Imprimir los parámetros
+        System.out.println(indentation + "\tParameters:");
+        for (Token param : params) {
+            System.out.println(indentation + "\t\t" + param.lexema);
+        }
+
+        System.out.println(indentation + "\tBody:");
+        body.print(indentation + "\t\t");
+    }
 }
