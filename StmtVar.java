33cebd3c450ee10ea8a6f3f31e375fdde359a992
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2024-01-07 21:52:50 -0600

    Actualización de StmtVar
    Se agrega la función print y toString para lograr la impresión del árbol

--- StmtVar.java
+++ StmtVar.java
@@ -8,4 +8,23 @@ public class StmtVar extends Statement {
         this.name = name;
         this.initializer = initializer;
     }
+    
+    @Override
+    public String toString() {
+        if (initializer != null) {
+            return "var " + name.lexema + " = " + initializer.toString() + ";";
+        } else {
+            return "var " + name.lexema + ";";
+        }
+    }
+
+    @Override
+    public void print(String indentation) {
+       System.out.print(indentation + "StmtVar: " + name.lexema);
+        if (initializer != null) {
+            System.out.print(" = ");
+            initializer.print(""); // Sin indentación adicional para el inicializador
+        }
+        System.out.println(";");
+    }
 }
