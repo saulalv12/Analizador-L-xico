42a469593acf67088a20aa970cec0e3ca9b5d0c8
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:02:06 -0600

    Update ExprLogical.java
    se modifica la clase de ExprLogical, agregando la identación y complementando algunas líneas de mensaje como el error o que no es soportado

--- ExprLogical.java
+++ ExprLogical.java
@@ -10,5 +10,38 @@ public class ExprLogical extends Expression{
         this.operator = operator;
         this.right = right;
     }
-}
 
+    @Override
+    public void print(String indentation) {
+       System.out.println(indentation + "ExprLogical");
+        System.out.println(indentation + "\tLeft:");
+        left.print(indentation + "\t\t");
+        System.out.println(indentation + "\tOperator: " + operator.lexema);
+        System.out.println(indentation + "\tRight:");
+        right.print(indentation + "\t\t");
+    }
+
+    
+    @Override
+    public Object evaluate(TablaSimbolos tablita) {
+        Object leftValue = left.evaluate(tablita);
+        Object rightValue = right.evaluate(tablita);
+         if (!(leftValue instanceof Boolean && rightValue instanceof Boolean)) {
+        throw new RuntimeException("Error, las operaciones lógicas solo pueden realizarse con valores booleanos");
+    }
+        
+            switch (operator.tipo)
+            {
+                case AND:
+                    return ((Boolean) leftValue && (Boolean) rightValue);
+                case OR:
+                    return ((Boolean) leftValue || (Boolean) rightValue);
+                default:
+                    throw new RuntimeException("Operador lógico no soportado: " + operator.lexema);
+            }
+        
+        
+        
+       
+    }
+}
