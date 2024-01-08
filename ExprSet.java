4f6eda7a549f10f2d35e9d1ffb1302b91ca665f2
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:05:19 -0600

    Update ExprSet.java
    Se modifica ExprSet poniendo identación y poniendo prints para saber si el dato es soportado o no

--- ExprSet.java
+++ ExprSet.java
@@ -10,4 +10,19 @@ public class ExprSet extends Expression{
         this.name = name;
         this.value = value;
     }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "ExprSet");
+        System.out.println(indentation + "\tObject:");
+        object.print(indentation + "\t\t");
+        System.out.println(indentation + "\tName: " + name.lexema);
+        System.out.println(indentation + "\tValue:");
+        value.print(indentation + "\t\t");
+    }
+
+    @Override
+    public Object evaluate(TablaSimbolos tablita) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
