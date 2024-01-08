d05dfe3b5aa89bfae8a29998e88fbbb30d086e9c
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-01-07 21:25:08 -0600

    Metodo toString y print

--- ExprAssign.java
+++ ExprAssign.java
@@ -8,4 +8,14 @@ public class ExprAssign extends Expression{
         this.name = name;
         this.value = value;
     }
+    @Override
+    public String toString() {
+        return "(= " + name.lexema + " " + value.toString() + ")";
+    }
+
+    @Override
+    public void print(String indentation) {
+        System.out.println(indentation + "ExprAssign: " + name.lexema);
+        value.print(indentation + "\t");
+    }
 }
