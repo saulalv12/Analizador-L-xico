4b28001c7462760dc33bcdf0cd7ff40b8ffe0b88
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2024-01-07 21:59:43 -0600

    Actualización de StmtLoop
    Se agrega la función "print" para lograr la impresión del árbol

--- StmtLoop.java
+++ StmtLoop.java
@@ -8,4 +8,14 @@ public class StmtLoop extends Statement {
         this.condition = condition;
         this.body = body;
     }
+    
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "StmtLoop");
+        System.out.println(indentation + "\tCondition:");
+        condition.print(indentation + "\t\t");
+
+        System.out.println(indentation + "\tBody:");
+        body.print(indentation + "\t\t");
+    }
 }
