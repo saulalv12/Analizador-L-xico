fcc19f24cd6ebc364ff083568727551fa077222f
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:24:36 -0600

    Update StmtBlock.java
    Modificamos este StmtBlock, poniendo algunas otras funciones como entrarAlcance o salirAlcance, lo cual nos ayuda a identificar cunado se hace un push o un pop dentro de la tabla global para tener una nueva tabla para poder declarar más variables, o donde acaba esa tabla para que las variables de esa tabla no puedan ser ocupadas en otras tablas.

--- StmtBlock.java
+++ StmtBlock.java
@@ -1,12 +1,52 @@
 package interprete;
 
-import javax.swing.plaf.nimbus.State;
 import java.util.List;
 
-public class StmtBlock extends Statement{
+public class StmtBlock extends Statement {
     final List<Statement> statements;
 
     StmtBlock(List<Statement> statements) {
         this.statements = statements;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Statement statement : statements) {
+            sb.append(statement.toString());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void print(String indentation) {
+        System.out.println(indentation + "StmtBlock");
+        for (Statement stmt : statements) {
+            stmt.print(indentation + "\t");
+        }
+    }
+
+    public Object evaluate(List<Statement> statements) {
+        return null;
+    }
+
+ @Override
+public Object evaluate(TablaSimbolos tablita) {
+    tablita.entrarAlcance();
+    Object returnValue = null;
+    try {
+        for (Statement stmt : statements) {
+            returnValue = stmt.evaluate(tablita);
+            if (returnValue instanceof StmtReturn) {
+                break;
+            }
+        }
+    } finally {
+        tablita.salirAlcance();
+    }
+    return returnValue;
 }
+
+
+}
+
