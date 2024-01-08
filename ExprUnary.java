92de05d2f6b4d737862a3c0b5b196c5baa56c9b8
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:16:58 -0600

    Update ExprUnary.java
    Se modifica ExprUnary terminndolo, poniendo identación, poniendo algunos prints de errores o de que el operador unitario no es soportado, ya que no estaba terminado por completo

--- ExprUnary.java
+++ ExprUnary.java
@@ -9,4 +9,37 @@ public class ExprUnary extends Expression{
         this.operator = operator;
         this.right = right;
     }
-}
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "ExprUnary");
+        System.out.println(indentation + "\tOperator: " + operator.lexema);
+        System.out.println(indentation + "\tRight:");
+        right.print(indentation + "\t\t");
+    }
+
+    @Override
+    public Object evaluate(TablaSimbolos tablita) {
+        Object expr = right.evaluate(tablita);
+        switch(operator.tipo){
+            case MINUS:
+                if (!(expr instanceof Double)) {
+                throw new RuntimeException("El operador '-' no se puede aplicar ya que no es un numero.");
+            }
+                if (expr instanceof Double) {
+                    return -(double)expr;
+                }
+            case BANG:
+                return !isTruthy(expr);
+            
+            
+            default:
+                 throw new RuntimeException("Operador unario no soportado: " + operator.tipo);
+        }
+        
+        
+    }
+    private boolean isTruthy(Object object) {
+    if (object == null) return false;
+    if (object instanceof Boolean) return (Boolean) object;
+    return true;
