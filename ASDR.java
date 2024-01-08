dec50bf441309f4981d7a3281b169758271d3843
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-01-07 20:13:00 -0600

    Implementación de los metodos void
    Se creó la clase ASDR que es el parser implementando los métodos void para cada una de las clases agregadas

---
+++ ASDR.java
@@ -0,0 +1,88 @@
+
+package interprete;
+import java.util.List;
+public class ASDR implements Parser{
+    public ASDR(List<Token> tokens){
+        
+    }
+    
+    public void StmtBlock(){
+        
+    }
+    
+    public void ExprAssign(){
+        
+    }
+    
+    public void ExprBinary(){
+        
+    }
+     
+    public void ExprCallFunction(){
+        
+    } 
+     public void Expression(){
+        
+    }
+    public void ExprGet(){
+        
+    }
+    public void ExprGrouping(){
+        
+    }
+    public void ExprLiteral(){
+        
+    }
+    public void ExprLogical(){
+        
+    } 
+    public void ExprSet(){
+        
+    }
+    public void ExprSuper(){
+        
+    }
+    public void ExprThis(){
+        
+    }
+    public void ExprUnary(){
+        
+    }
+    public void ExprVariable(){
+        
+    }
+     public void Statement(){
+        
+    }
+     public void StmtClass(){
+        
+    }
+     
+    public void StmtExpression(){
+        
+    }
+    public void StmtFunction(){
+        
+    }
+    public void Stmtlf(){
+        
+    }
+    public void StmtLoop(){
+        
+    }
+     public void StmtPrint(){
+        
+    }
+     
+    public void StmtReturn(){
+        
+    } 
+    public void StmtVar(){
+        
+    }
+    @Override
+    public boolean parse() {
+       return true;
+    }
+    
+}
