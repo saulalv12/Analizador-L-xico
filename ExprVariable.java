7b72352be7af8018c81cdd13d4d4d3516a99dc3d
GXMXZ <121576862+GXMXZ@users.noreply.github.com>
2024-01-07 22:20:33 -0600

    Update ExprVariable.java
    Modificamos ExprVariable completando su trabajo ya que era muy básico lo que hacia, agregamos identación y los prints correspondientes de los resultados que nos darían dichas expresiones, en caso de no ser encontrado, se muestra un mensaje de que la variable no a sido definida

--- ExprVariable.java
+++ ExprVariable.java
@@ -2,8 +2,33 @@ package interprete;
 
 class ExprVariable extends Expression {
     final Token name;
-
-    ExprVariable(Token name) {
+   
+    
+    ExprVariable(Token name, TablaSimbolos tablita) {
         this.name = name;
+        
     }
+    @Override
+    public String toString() {
+        return name.lexema;
+    }
+
+    @Override
+    public void print(String indentation) {
+        System.out.println(indentation + "ExprVariable: " +  name.lexema);
+    }
+
+    @Override
+public Object evaluate(TablaSimbolos tablita) {
+    
+    if (tablita.existeIdentificador(name.lexema)) {
+        return tablita.obtener(name.lexema); 
+    } else {
+        throw new RuntimeException("La variable '" + name.lexema + "' no está definida.");
+    }
+}
+public String getName(){
+    return name.lexema;
+}
+
 }
