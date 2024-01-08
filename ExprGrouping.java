cada621424acfda9714648266def14ec02eca635
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-01-07 21:32:43 -0600

    método abstracto print
    Se modifica el método abstacto para que imprima dentro del arbol

--- ExprGrouping.java
+++ ExprGrouping.java
@@ -6,4 +6,9 @@ public class ExprGrouping extends Expression {
     ExprGrouping(Expression expression) {
         this.expression = expression;
     }
+    @Override
+    public void print(String indentation) {
+        System.out.println(indentation + "ExprGrouping");
+        expression.print(indentation + "\t");
+    }
 }
