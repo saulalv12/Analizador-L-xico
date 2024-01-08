e68ba8bd480da980ee77fe2236cc5060901a7daf
saulalv12 <135563438+saulalv12@users.noreply.github.com>
2024-01-07 21:55:45 -0600

    Actualización de StmtPrint
    Se agrega la función "print" y "toString" para lograr la impresión del árbol

--- StmtPrint.java
+++ StmtPrint.java
@@ -7,4 +7,15 @@ public class StmtPrint extends Statement {
     StmtPrint(Expression expression) {
         this.expression = expression;
     }
+    
+    @Override
+    public String toString() {
+        return expression.toString(); 
+    }
+
+    @Override
+    public void print(String indentation) {
+         System.out.println(indentation + "StmtPrint");
+        expression.print(indentation + "\t");
+    }
 }
